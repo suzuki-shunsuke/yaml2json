4f7dcae3d3903c616999ca19cad5c321710f6e2d
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-05-30 12:48:53 +0900

    feat: support indent

--- pkg/cli/runner.go
+++ pkg/cli/runner.go
@@ -32,9 +32,10 @@ func (flags *LDFlags) ShowVersion() string {
 	return flags.Version + " (" + flags.Commit + ")"
 }
 
-func parseFlag(verFlag, helpFlag *bool) {
+func parseFlag(verFlag, helpFlag *bool, indentFlag *string) {
 	pflag.BoolVarP(verFlag, "version", "v", false, "show yaml2json's version")
 	pflag.BoolVarP(helpFlag, "help", "h", false, "show the help message")
+	pflag.StringVarP(indentFlag, "indent", "i", "", "indent")
 	pflag.Parse()
 }
 
@@ -44,14 +45,15 @@ const helpMsg = `NAME:
    https://github.com/suzuki-shunsuke/yaml2json
 
 USAGE:
-   yaml2json [--help, -h] [--version, -v]
+   yaml2json [--help, -h] [--version, -v] [--indent, -i ""]
 
 VERSION:
    %s
 
 OPTIONS:
    --help, -h           show help (default: false)
-   --version, -v        print the version (default: false)`
+   --version, -v        print the version (default: false)
+	 --indent, -i         indent (default: "")`
 
 var errFileRequired = errors.New("YAML file is required")
 
@@ -59,7 +61,7 @@ func (runner *Runner) Run(ctx context.Context, args ...string) error {
 	ctrl := controller.New(os.Stdout)
 	param := &controller.RunParam{}
 	verFlag, helpFlag := false, false
-	parseFlag(&verFlag, &helpFlag)
+	parseFlag(&verFlag, &helpFlag, &param.Indent)
 	if helpFlag {
 		fmt.Fprintf(runner.Stdout, helpMsg, runner.LDFlags.ShowVersion())
 		return nil
